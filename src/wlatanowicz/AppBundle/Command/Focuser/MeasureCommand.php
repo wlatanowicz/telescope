c54e306daa149e9e151956be05c118812ee4e470
Wiktor Latanowicz <wlatanowicz@flux.pl>
2016-12-28 15:18:00 +0100

    focus measure command

---
+++ src/wlatanowicz/AppBundle/Command/Focuser/MeasureCommand.php
@@ -0,0 +1,124 @@
+<?php
+declare(strict_types = 1);
+
+namespace wlatanowicz\AppBundle\Command\Focuser;
+
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use wlatanowicz\AppBundle\Data\AutofocusPoint;
+use wlatanowicz\AppBundle\Data\AutofocusResult;
+use wlatanowicz\AppBundle\Hardware\Provider\FocuserProvider;
+use wlatanowicz\AppBundle\Hardware\Provider\ImagickCroppedCameraProvider;
+use wlatanowicz\AppBundle\Routine\AutoFocusReport;
+use wlatanowicz\AppBundle\Routine\MeasureStarFWHM;
+
+class MeasureCommand extends Command
+{
+    /**
+     * @var ImagickCroppedCameraProvider
+     */
+    private $cameraProvider;
+
+    /**
+     * @var FocuserProvider
+     */
+    private $focuserProvider;
+
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    /**
+     * MeasureCommand constructor.
+     * @param ImagickCroppedCameraProvider $cameraProvider
+     * @param FocuserProvider $focuserProvider
+     */
+    public function __construct(
+        ImagickCroppedCameraProvider $cameraProvider,
+        FocuserProvider $focuserProvider,
+        LoggerInterface $logger
+    ) {
+        parent::__construct(null);
+
+        $this->cameraProvider = $cameraProvider;
+        $this->focuserProvider = $focuserProvider;
+        $this->logger = $logger;
+    }
+
+
+    protected function configure()
+    {
+        $this
+            ->setName('focuser:measure')
+            ->setDescription('Creates new users.')
+            ->setHelp("This command allows you to create users...")
+            ->addOption('camera', null, InputOption::VALUE_REQUIRED, 'Camera name', 'sim')
+            ->addOption('time', null, InputOption::VALUE_REQUIRED, 'Target position', 4)
+            ->addOption('focuser', null, InputOption::VALUE_REQUIRED, 'Target position', 'sim')
+            ->addOption('x', null, InputOption::VALUE_REQUIRED, 'Target position', null)
+            ->addOption('y', null, InputOption::VALUE_REQUIRED, 'Target position', null)
+            ->addOption('radius', 'r', InputOption::VALUE_REQUIRED, 'Target position', 40)
+            ->addOption('threshold', null, InputOption::VALUE_REQUIRED, 'Target position', 0.1)
+            ->addOption('save-report', null, InputOption::VALUE_REQUIRED, 'Target position', null)
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $cameraName = $input->getOption('camera');
+        $focuserName = $input->getOption('focuser');
+
+        $time = intval($input->getOption('time'), 10);
+
+        $x = $input->getOption('x') !== null
+            ? intval($input->getOption('x'), 10)
+            : null;
+        $y = $input->getOption('y') !== null
+            ? intval($input->getOption('y'), 10)
+            : null;
+
+        $radius = intval($input->getOption('radius'), 10);
+
+        $threshold = floatval($input->getOption('threshold'));
+
+        $reportFile = $input->getOption('save-report');
+
+        $camera = $this->cameraProvider->getCamera($cameraName);
+        $focuser = $this->focuserProvider->getFocuser($focuserName);
+
+        $camera->setCroping(
+            $radius,
+            $x,
+            $y
+        );
+
+        $measure = new MeasureStarFWHM($threshold);
+
+        $image = $camera->exposure($time);
+
+        $measureValue = $measure->measure($image);
+
+        $point = new AutofocusPoint(
+            $focuser->getPosition(),
+            $measureValue,
+            $image
+        );
+
+        $result = new AutofocusResult(
+            $point,
+            [$point]
+        );
+
+        echo "Measured value: {$measureValue}\n";
+
+        if ($reportFile !== null) {
+            $reporter = new AutoFocusReport();
+            $report = $reporter->generateReport($result);
+            file_put_contents($reportFile, $report->getImageBlob());
+        }
+    }
+}
