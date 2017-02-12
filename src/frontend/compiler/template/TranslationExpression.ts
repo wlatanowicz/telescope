7e64dc6584c5e91367087f92d761a6503c2cc3f0
Wiktor Latanowicz <wlatanowicz@flux.pl>
2017-02-12 20:43:21 +0100

    tomek.js as npm package

--- src/frontend/compiler/template/TranslationExpression.ts
+++
@@ -1,9 +0,0 @@
-export default class TranslationExpression {
-
-	text: string;
-
-	constructor( str:string ){
-		this.text = str;
-	}
-
-}
--- src/frontend/framework/WebControls/ValidationControls/_doc.js
+++
@@ -1,4 +0,0 @@
-/**
- * == WebControls_ValidationControls ==
- **/
-
